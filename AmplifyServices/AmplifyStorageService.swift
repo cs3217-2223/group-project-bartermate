afdd0970947ea73486d3cb0b33c5ceca2e5d934c
Zico <zicotjia@gmail.com>
2023-04-17 00:44:29 +0800

    fix last minute bug

--- AmplifyServices/AmplifyStorageService.swift
+++ AmplifyServices/AmplifyStorageService.swift
@@ -11,19 +11,19 @@ import Foundation
 public class AmplifyStorageService: StorageService {
 
     func uploadImage(key: String, _ data: Data) -> StorageUploadDataTask {
-        let options = StorageUploadDataRequest.Options(accessLevel: .protected)
+        let options = StorageUploadDataRequest.Options(accessLevel: .guest)
         return Amplify.Storage.uploadData(key: key,
                                           data: data,
                                           options: options)
     }
 
     func downloadImage(key: String) -> StorageDownloadDataTask {
-        let options = StorageDownloadDataRequest.Options(accessLevel: .protected)
+        let options = StorageDownloadDataRequest.Options(accessLevel: .guest)
         return Amplify.Storage.downloadData(key: key, options: options)
     }
 
     func removeImage(key: String) async throws -> String {
-        let options = StorageRemoveRequest.Options(accessLevel: .protected)
+        let options = StorageRemoveRequest.Options(accessLevel: .guest)
         return try await Amplify.Storage.remove(key: key, options: options)
     }
 
